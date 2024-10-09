90004da0ef21668bfcd4058287496302587c3d3f
luanasj <luanasj@outlook.com.br>
2024-10-09 17:40:39 -0300

    definition for function to edge rag or generate

--- AdaptativeRAG/tests.py
+++ AdaptativeRAG/tests.py
@@ -11,7 +11,7 @@ from langchain_groq import ChatGroq
 from pydantic import BaseModel, Field
 
 # Data model
-class Decide_to_RAG(BaseModel):
+class rag_or_generate(BaseModel):
     """
     Invokes the agent model to generate a response based on the current state. Given
     the question, it will decide to route retrieve using the retriever tool, or simply generate.
@@ -29,7 +29,7 @@ class Decide_to_RAG(BaseModel):
 
 # LLM with function call
 llm = ChatGroq(model="llama3-8b-8192",temperature=0)
-structured_llm_decision_maker = llm.with_structured_output(Decide_to_RAG)
+structured_llm_decision_maker = llm.with_structured_output(rag_or_generate)
 
 # Prompt
 system = """You are an expert at deciding, based on a question, to retrieve or generate an answer.
@@ -79,67 +79,29 @@ while (question != "end"):
 
 ##Create rag_or_generate router
 
-# def rag_or_generate():
-#     """
-#     Given a question, it will decide retrieve using the retrieving tools,
-#     or simply generate an answer.
+def rag_or_generate(state):
+    """
+    Given a question, it will decide retrieve using the retrieving tools,
+    or simply generate an answer.
 
-#     Args:
-#         state (question): user question
+    Args:
+        state (question): user question
 
-#     Returns:
-#         str: Next node to call
-#     """
-#     print("---ROUTE QUESTION---")
-#     question = state["question"]
-#     source = question_router.invoke({"question": question})
-#     if source.method == "generate":
-#         print("---ROUTE QUESTION TO GENERATE---")
-#         return "generate"
-#     elif source.method == "route":
-#         print("---ROUTE QUESTION TO RETRIEVE---")
-#         return "route"
+    Returns:
+        str: Next node to call
+    """
+    print("---ROUTE QUESTION---")
+    question = state["question"]
+    source = agent_router.invoke({"question": question})
+    if source.method == "generate":
+        print("---ROUTE QUESTION TO GENERATE---")
+        return "generate"
+    elif source.method == "retrieve":
+        print("---ROUTE QUESTION TO RETRIEVE---")
+        return "retrieve"
 
 
-# def agent(state):
-#     """
-#     Invokes the agent model to generate a response based on the current state. Given
-#     the question, it will decide to retrieve using the retriever tool, or simply end.
 
-#     Args:
-#         state (dict): The current graph state
-
-#     Returns:
-#         dict: The updated state with the agent response appended to messages
-#     """
-#     print("---CALL AGENT---")
-#     messages = state["messages"]
-#     model = ChatOpenAI(temperature=0, streaming=True, model="gpt-4-turbo")
-#     model = model.bind_tools(tools)
-#     response = model.invoke(messages)
-#     # We return a list, because this will get added to the existing list
-#     return {"messages": [response]}
-
-# def route_question(state):
-#     """
-#     Route question to web search or RAG.
-
-#     Args:
-#         state (dict): The current graph state
-
-#     Returns:
-#         str: Next node to call
-#     """
-
-#     print("---ROUTE QUESTION---")
-#     question = state["question"]
-#     source = question_router.invoke({"question": question})
-#     if source.datasource == "web_search":
-#         print("---ROUTE QUESTION TO WEB SEARCH---")
-#         return "web_search"
-#     elif source.datasource == "vectorstore":
-#         print("---ROUTE QUESTION TO RAG---")
-#         return "vectorstore"
 
 
 
