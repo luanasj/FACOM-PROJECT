74e43be8e6637e1c004e0e43a0273e4b97f19e30
luanasj <luanasj@outlook.com.br>
2024-11-08 17:33:09 -0300

    atualizando funcao de extracao de informacoes para rag no tests.py

--- AdaptativeRAG/tests.py
+++ AdaptativeRAG/tests.py
@@ -2,10 +2,102 @@ from dotenv import load_dotenv
 load_dotenv()
 # #generation prompt "luanasjdev/rag-prompt-with-chat-history"
 
-# ###RETRIEVER
+import requests
+import os
 
-# from dotenv import load_dotenv
-# load_dotenv()
+### Download pdfs
+def baixar_pdf(url, pasta_destino, nome_arquivo):
+    # Verifica se a pasta de destino existe, se não, cria a pasta
+    if not os.path.exists(pasta_destino):
+        os.makedirs(pasta_destino)
+    
+    # Faz o download do PDF
+    resposta = requests.get(url, verify=False)
+    
+    # Caminho completo do arquivo
+    caminho_arquivo = os.path.join(pasta_destino, f"{nome_arquivo}")
+    
+    # Salva o PDF na pasta de destino
+    with open(caminho_arquivo, 'wb') as pdf_file:
+        pdf_file.write(resposta.content)
+    
+    print(f"PDF salvo em: {caminho_arquivo}")
+
+# PDFs to index
+
+pdflinks = [{"link":f"https://facom.ufba.br/portal/conteudo/files/EDITAL%20PROEXT%202024.pdf","title":"pdf1.pdf"},{"link":f"https://facom.ufba.br/portal/conteudo/files/Guia%20do%20semestre%20para%20estudantes%20da%20FACOM%202024-2.pdf","title":"pdf2.pdf"}]
+
+pasta_destino_pdfs = r'C:\Users\luana\OneDrive\Documentos\FACOM-Project\Agents\temp'
+
+for item in pdflinks:
+    baixar_pdf(item["link"], pasta_destino_pdfs, item["title"])
+
+
+# Web pages to index
+urls = [
+    "https://facom.ufba.br/portal/informes/787/confira-resultado-final-da-selecao-para-estagio-remunerado-na-facom-ufba",
+    "https://facom.ufba.br/portal/informes/783/facom-recebe-novos-estudantes-com-programacao-de-acolhimento-",
+    "https://facom.ufba.br/portal/informes/777/departamento-de-comunicacao-abre-inscricoes-para-selecao-de-monitores",
+]
+
+### Build Index
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+# from langchain_openai import OpenAIEmbeddings
+
+from langchain_cohere import CohereEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+
+# Set embeddings
+embd = CohereEmbeddings(model='embed-english-v3.0')
+
+
+#docs
+docs = [WebBaseLoader(url).load() for url in urls] + [PyPDFLoader(f"../temp/{pdf}.pdf").load() for pdf in pdflinks["title"]]
+
+# Load
+docs_list = [item for sublist in docs for item in sublist]
+
+# Split
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=500, chunk_overlap=0
+)
+doc_splits = text_splitter.split_documents(docs_list)
+
+# Add to vectorstore
+vectorstore = Chroma.from_documents(
+    documents=doc_splits,
+    collection_name="rag-chroma",
+    embedding=embd,
+)
+retriever = vectorstore.as_retriever()
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Exemplo de uso
+# url_pdf = f"https://facom.ufba.br/portal/conteudo/files/Guia%20do%20semestre%20para%20estudantes%20da%20FACOM%202024-2.pdf"
+# pasta_destino = r'C:\Users\luana\OneDrive\Documentos\FACOM-Project\Agents\temp'
+# nome_arquivo = 'arquivoSiteFacom'
+
+
+
+# baixar_pdf(url_pdf, pasta_destino, nome_arquivo)
+
+
+
+### Download pdfs
 
 # ### Build Index
 
@@ -64,49 +156,49 @@ load_dotenv()
 
 # print(docs[0].metadata)
 
-from langchain_community.document_loaders import PyPDFLoader
+# from langchain_community.document_loaders import PyPDFLoader
 
-# Specify the URL of the PDF
-# pdf_url = f"https://facom.ufba.br/portal/conteudo/files/Guia%20do%20semestre%20para%20estudantes%20da%20FACOM%202024-2.pdf"
+# # Specify the URL of the PDF
+# # pdf_url = f"https://facom.ufba.br/portal/conteudo/files/Guia%20do%20semestre%20para%20estudantes%20da%20FACOM%202024-2.pdf"
 
-pdf_url = f"https://facom.ufba.br/portal/conteudo/files/Guia%20do%20semestre%20para%20estudantes%20da%20FACOM%202024-2.pdf"
+# pdf_url = f"../temp/{nome_arquivo}.pdf"
 
-# Create a loader instance
-loader = PyPDFLoader(pdf_url)
+# # Create a loader instance
+# loader = PyPDFLoader(pdf_url)
 
-# Load the PDF pages
-# pages = []
-# async for page in loader.alazy_load():
-#     pages.append(page)
-docs = loader.load()
+# # Load the PDF pages
+# # pages = []
+# # async for page in loader.alazy_load():
+# #     pages.append(page)
+# docs = loader.load()
 
-# Print the content of the first page
-print(docs[0].metadata)
-print(docs[0])
+# # Print the content of the first page
+# print(docs[1].metadata)
+# print(docs[1])
 
-#Baixar pdfs em pasta do servidor para cnsumir 
+# #Baixar pdfs em pasta do servidor para cnsumir 
 
-# import requests
-# from bs4 import BeautifulSoup
+# # import requests
+# # from bs4 import BeautifulSoup
 
-# # URL da página que contém os links dos PDFs
-# url = 'https://exemplo.com/pagina-com-pdfs'
+# # # URL da página que contém os links dos PDFs
+# # url = 'https://exemplo.com/pagina-com-pdfs'
 
-# # Fazer a requisição para a página
-# response = requests.get(url)
-# soup = BeautifulSoup(response.content, 'html.parser')
+# # # Fazer a requisição para a página
+# # response = requests.get(url)
+# # soup = BeautifulSoup(response.content, 'html.parser')
 
-# # Encontrar todos os links de PDF na página
-# pdf_links = soup.find_all('a', href=True)
-# pdf_links = [link['href'] for link in pdf_links if link['href'].endswith('.pdf')]
+# # # Encontrar todos os links de PDF na página
+# # pdf_links = soup.find_all('a', href=True)
+# # pdf_links = [link['href'] for link in pdf_links if link['href'].endswith('.pdf')]
 
-# # Baixar cada PDF
-# for link in pdf_links:
-#     pdf_response = requests.get(link)
-#     pdf_name = link.split('/')[-1]
-#     with open(pdf_name, 'wb') as pdf_file:
-#         pdf_file.write(pdf_response.content)
-#     print(f'{pdf_name} baixado com sucesso!')
+# # # Baixar cada PDF
+# # for link in pdf_links:
+# #     pdf_response = requests.get(link)
+# #     pdf_name = link.split('/')[-1]
+# #     with open(pdf_name, 'wb') as pdf_file:
+# #         pdf_file.write(pdf_response.content)
+# #     print(f'{pdf_name} baixado com sucesso!')
 
 
 
