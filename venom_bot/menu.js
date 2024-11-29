cb42fa6951ffa3644624a18c682c84175b98549c
luanasj <luanasj@outlook.com.br>
2024-11-29 17:39:17 -0300

    fazendo menu

---
+++ venom_bot/menu.js
@@ -0,0 +1,25 @@
+import dados from '../externalInfo.json' assert { type: 'json' };
+
+const selectorStore = dados
+
+console.log(selectorStore)
+
+// const isMenuSelector = (message,selectorStore)=>{
+//     let selectors = selectorStore.keys
+
+//     return selectors.includes(message)
+    
+// }
+
+// const menuSelectorContent = (selector)=>{
+//     let message = [1,7,8,9]
+
+// }
+
+// console.log(selectorStore.)
+
+
+
+
+
+
