a3b9c77de43fadb31556d82c65b1c9403e98cb38
luanasj <luanasj@outlook.com.br>
2024-12-04 18:29:07 -0300

    construida funcao para adicao de menu na geração de mensagens sem rag no arquivo testes

--- AdaptativeRAG/tests.js
+++
@@ -1,8 +0,0 @@
-fetch("http://127.0.0.1:5000/luana",{
-    method:"POST",
-    headers: { 'Content-Type': 'application/json' },
-    body:JSON.stringify({
-        pergunta:"eu sou uma pergunta"
-    })
-}).then(data=>data.text())
-.then(res=>console.log(res))
--- AdaptativeRAG/tests.py
+++ AdaptativeRAG/tests.py
@@ -1,20 +1,51 @@
-from flask import Flask,request
+from dotenv import load_dotenv
+load_dotenv()
 
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
-
-from markupsafe import escape
-
-@app.route("/<name>",methods=["GET","POST"])
-def hello(name):
-    return f"Hello, {escape(name)} \n request info: {request.get_json().get('pergunta')} !"
+import json
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
 
 
+def getSelectorsFromJSON(jsonPath):
+    with open(jsonPath, 'r',encoding='utf-8') as arquivo:
+        dadosJSON = json.load(arquivo)
+
+    selectors = ""
+
+    for i in range(len(dadosJSON)):
+        item = dadosJSON[i]
+        selectors += f"{i+1} {item['name']}\n"
+
+    selectors += f"{len(dadosJSON)+1} Outros" 
+
+    print(selectors)
+
+llm = ChatGroq(model="llama3-8b-8192",temperature=0)
+
+no_rag_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            f'''Você é um assistente preparado para responder informacoes sobre a FACOM (Faculdade de Comunicação) da UFBA.
+
+            Caso seja a primeira mensagem do usuário ou uma saudação como "olá", "bom dia", "você pode me ajudar","tenho uma dúvida", peça que o usuário escolha uma das opcções do menu abaixo digitando o número da opção desejada. 
+
+            
+            {getSelectorsFromJSON('../externalInfo.json')}
+            
+            
+            ''',
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
 
 
+# Chain
+no_rag_chain = no_rag_prompt | llm | StrOutputParser()
+
+print(no_rag_chain.invoke({"messages":[input()]}))
 
 
 
